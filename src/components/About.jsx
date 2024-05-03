1021d81ce82690a5bae4f7ba5d64042002e75568
JSEAN6 <seannjoroge0@gmail.com>
2024-05-03 11:53:58 +0300

    add files

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import travel from '/src/assets/travel.jpg'
-import '../About.css'
+import travel from '/src/assets/travel.jpg';
+import '../About.css';
 
 function About() {
     return (
         <div className="body">
-                <img src={travel} className="image"/>
+            <img src={travel} className="image" alt="Travel"/>
             <h1>About Us</h1>
             <p>
-                Welcome to GlobeTrekker Getaways, your ultimate destination for unforgettable travel experiences and seamless accommodation bookings.
-                At GlobeTrekker Getaways, we're passionate about helping travelers discover new destinations, immerse themselves in diverse cultures, and create lasting memories.
+                Welcome to <strong>SavannahSunsetTours</strong>, your ultimate destination for unforgettable travel experiences and seamless accommodation bookings.
+                At <strong>SavannahSunsetTours</strong>, we're passionate about helping travelers discover new destinations, immerse themselves in diverse cultures, and create lasting memories.
 
                 Our team consists of avid travelers and hospitality experts dedicated to curating exceptional travel experiences tailored to your preferences and budget.
                 Whether you're seeking a luxurious resort getaway, a cozy bed and breakfast retreat, or an adventurous expedition off the beaten path, we've got you covered.
@@ -20,7 +20,7 @@ function About() {
                 But we're more than just a booking platform – we're your trusted travel companion every step of the way.
                 From expert travel guides and insider tips to personalized recommendations and 24/7 customer support, we're here to make your journey seamless and stress-free.
 
-                Whether you're planning a romantic getaway, a family vacation, or a solo adventure, let GlobeTrekker Getaways be your gateway to the world's most captivating destinations.
+                Whether you're planning a romantic getaway, a family vacation, or a solo adventure, let <strong>SavannahSunsetTours</strong> be your gateway to the world's most captivating destinations.
                 Join us on a journey of discovery, exploration, and unforgettable experiences. Your next adventure starts here.
             </p>
         </div>
@@ -28,3 +28,4 @@ function About() {
 };
 
 export default About;
+
